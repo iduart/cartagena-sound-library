81f9eab08bf5c8c812470be4c09478350a68a5a8
Iduart <idean@buildingengines.com>
2020-07-19 21:31:32 -0500

    share audio

--- screens/SoundDetail.js
+++ screens/SoundDetail.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import * as Sharing from 'expo-sharing';
+import { Asset } from 'expo-asset';
 import { TouchableWithoutFeedback, View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/Entypo';
 import LogoHeader from '../components/LogoHeader';
 import SoundItem from '../components/SoundItem';
+import Sounds from '../media';
 
 const SafeAreaContainer = styled(SafeAreaView)`
   flex: 1;
 `;
 
-const IconsContainer = styled.View`
+const IconContainer = styled.View`
+  align-items: flex-end;
+  padding-right: 10px;
+  position: relative;
+  top: -15px;
+`;
+
+const IconWithLabelContainer = styled.View`
   flex-direction: row;
-  justify-content: space-around;
+  align-items: center;
+`;
+
+const IconLabel = styled.Text`
+  font-size: 18px;
 `;
 
 const ICON_SIZE = 35;
@@ -30,6 +44,21 @@ const SoundDetail = ({ navigation, route }) => {
 
   const goHome = () => navigation.navigate('home');
 
+  const shareAudio = async () => {
+    const sound = Sounds[params.code];
+    const canShare = await Sharing.isAvailableAsync();
+    if (canShare) {
+      await Asset.fromModule(sound).downloadAsync();
+      const localUri = await Asset.fromModule(sound).localUri;
+
+      Sharing.shareAsync(localUri, {
+        mimeType: 'audio/mpeg',
+        dialogTitle: params.text,
+        UTI: 'public.mp3',
+      })
+    }
+  }
+
   return (
     <SafeAreaContainer>
       <TouchableWithoutFeedback onPress={goHome}>
@@ -37,12 +66,15 @@ const SoundDetail = ({ navigation, route }) => {
           <LogoHeader />
         </View>
       </TouchableWithoutFeedback>
-      <SoundItem sound={{...params}} />
-      <IconsContainer>
-        <Icon name="whatsapp" size={ICON_SIZE} />
-        <Icon name="slack" size={ICON_SIZE} />
-        <Icon name="download" size={ICON_SIZE} />
-      </IconsContainer>
+      <SoundItem sound={{ ...params }} />
+      <IconContainer>
+        <TouchableWithoutFeedback onPress={shareAudio}>
+          <IconWithLabelContainer>
+            <Icon name="share" size={ICON_SIZE} />
+            <IconLabel>Compartir</IconLabel>
+          </IconWithLabelContainer>
+        </TouchableWithoutFeedback>
+      </IconContainer>
       <TouchableWithoutFeedback onPress={goHome}>
         <Footer>
           <Icon name="home" size={40} color="#FF6700" /><Text>Inicio</Text>
