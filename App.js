626774e122cacb158cb168452b142bfc80d71961
Iduart <idean@buildingengines.com>
2020-07-26 22:03:38 -0500

    drawer navigation

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import DrawerContent from './components/DrawerContent';
 import Home from './screens/Home';
 
-const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
+      <Drawer.Navigator
+        drawerContent={DrawerContent}
+        drawerStyle={{ 
+          width: '85%',
+          backgroundColor: '#F37578',
+          borderRadius: 15,
+        }}
+      >
         <Drawer.Screen name="Home" component={Home} />
       </Drawer.Navigator>
-      {/* <Stack.Navigator initialRouteName="home" headerMode="none">
-        <Stack.Screen name="home" component={Home} />
-        <Stack.Screen name="soundDetail" component={SoundDetail} />
-      </Stack.Navigator> */}
     </NavigationContainer>
   );
 }
